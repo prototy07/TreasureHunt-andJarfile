3ae6c77a2fd2ac3d26fe32e65fc4485d38d14591
chip <lnwchipza@gmail.com>
2022-11-25 21:08:30 +0700

    first commit

---
+++ src/my2dgame/My2DGame.java
@@ -0,0 +1,27 @@
+package my2dgame;
+
+import javax.swing.JFrame;
+
+public class My2DGame {
+
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Treasure Hunt");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.setupGame();
+        gamePanel.startGameThread();
+
+    }
+
+}
